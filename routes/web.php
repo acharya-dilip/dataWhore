4b34befd283bee780216058a36002ab5b13f22d5
acharya-dilip <acharyamission6@gmail.com>
2026-07-09 12:52:54 +0545

    created a upload rotue

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,12 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::prefix('dashboard')->group(function () {
+    Route::get('upload',function(){
+        return Inertia::render('Upload');
+    })->name('upload.index');
+});
+
+
+
 require __DIR__.'/auth.php';
