eb810484034f0908cd433be5821fedb85fe9dbb7
acharya-dilip <acharyamission6@gmail.com>
2026-07-10 10:19:59 +0545

    made the pdf files visible too

--- resources/js/Components/FileCard.tsx
+++ resources/js/Components/FileCard.tsx
@@ -1,20 +1,34 @@
 import {Link} from "@inertiajs/react";
-import {LucideEye} from "lucide-react";
+import {LucideEye, TrashIcon} from "lucide-react";
 import {useState} from "react";
 import FilePreview from "@/Components/FilePreview";
 
 export default function FileCard({file}:{file:any}) {
+
+    console.log(route('file.view', {id: file.id}))
 
 
     return(
         <>
             <div className={"w-full bg-cyan-200 rounded-lg h-max flex flex-col gap-2 items-center justify-end p-2"}>
                 <div className={"h-[25vh] w-full overflow-hidden"}>
-                    <img
-                        alt={"anImage"}
-                        className={" h-full w-full"}
-                        src={route('file.view', {id: file.id})}
-                    />
+
+                    {file.mime.startsWith('image/') && (
+                        <img
+                            alt={"anImage"}
+                            className={" h-full w-full"}
+                            src={route('file.view', {id: file.id})}
+                        />
+                    )}
+                    {file.mime==="application/pdf" &&(
+                        <iframe
+                            className={" h-full w-full border-none overflow-hidden pointer-events-none"}
+                            src={route('file.view', {id: file.id})+"#toolbar=0&navpanes=0&scrollbar=0"}
+                            scrolling="no"
+                        />
+                    )
+
+                    }
 
                 </div>
                 <div className={"text-xl flex justify-between w-full  "}>
@@ -23,8 +37,9 @@ export default function FileCard({file}:{file:any}) {
                     </div>
                     <Link href={"#"}>
                         <button
+                            className={"p-2 bg-white rounded-full group hover:bg-red-600"}
                         >
-                            <LucideEye className={"inline-block hover:text-gray-400"}/>
+                            <TrashIcon className={"inline-block size-7 text-red-600 group-hover:text-white  rounded-full"}/>
                         </button>
                     </Link>
                 </div>
