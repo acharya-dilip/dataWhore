42d7e1328d229dfcdf521df3c2fa75f2008ee540
acharya-dilip <acharyamission6@gmail.com>
2026-07-10 11:46:41 +0545

    fixed the post request too large issue

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\FileRequest;
 use App\Models\File;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
