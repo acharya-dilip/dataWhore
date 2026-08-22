3ea3e85e9fa31fa1284889c3f6bb38497bc4e1f8
acharya-dilip <acharyamission6@gmail.com>
2026-08-22 14:31:00 +0545

    .

--- database/migrations/2026_08_22_084143_remove_path_from_folders.php
+++ database/migrations/2026_08_22_084143_remove_path_from_folders.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('folders', function (Blueprint $table) {
-            //
+
+            $table->dropColumn('path');
+
         });
     }
 
